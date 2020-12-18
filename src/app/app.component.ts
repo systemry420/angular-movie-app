09253b3b93fa3e4d62f33319cdc826fb845f68c6
Hasan <systemry420@gmail.com>
2020-12-18 19:14:10 +0200

    pass data to template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'movie-app';
+  APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
+
+  IMGPATH = 'https://image.tmdb.org/t/p/w1280';
+
+  movies
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get(this.APIURL).subscribe(data => {
+      console.log(data);
+      this.movies = data.results
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MovieComponent } from './movie/movie.component';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
     AppComponent,
-    MovieComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
