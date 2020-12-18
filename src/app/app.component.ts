39f78afedfbfc8e0a5c44e5995b2308d53da44f1
Hasan <systemry420@gmail.com>
2020-12-18 19:53:33 +0200

    fetch movies by default and on search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,15 +10,27 @@ export class AppComponent {
   title = 'movie-app';
   APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
 
+  public searchTerm = ''
   IMGPATH = 'https://image.tmdb.org/t/p/w1280';
+  SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=';
 
   movies
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.http.get(this.APIURL).subscribe(data => {
-      console.log(data);
+    this.fetchMovies(this.APIURL)
+  }
+
+  fetchMovies(url) {
+    this.http.get(url).subscribe(data => {
+      // console.log(data);
       this.movies = data.results
     })
+  }
+
+  searchMovie(val) {
+    if(val) {
+      this.fetchMovies(this.SEARCHAPI + val)
+    }
   }
 }
